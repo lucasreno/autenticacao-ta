57b77995dfe082ef9ff468437ae09c10a6551c7f
Lucas Renó <lucasreno9@gmail.com>
2022-10-26 22:39:29 -0300

    Criado auth service, user interface e login html

---
+++ src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDQUnP6YSJBVrzbPUNX9eH12pBFAw5d6ys",
+    authDomain: "autenticacao-5bb58.firebaseapp.com",
+    projectId: "autenticacao-5bb58",
+    storageBucket: "autenticacao-5bb58.appspot.com",
+    messagingSenderId: "702669096063",
+    appId: "1:702669096063:web:85e085e92fa68614f12b4a"
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
